dde3fae5c484c1467dde858c446dac5e3da8491b
Jason Petersen <jason@citusdata.com>
2017-09-21 13:49:18 -0700

    Prettify node name display

--- lib/pghero/methods/citus.rb
+++ lib/pghero/methods/citus.rb
@@ -26,7 +26,16 @@ module PgHero
       end
 
       def nodes
-        select_all("SELECT node_name FROM master_get_active_worker_nodes()")
+        select_all <<-SQL
+          SELECT placements.nodename AS name,
+                 nodes.isactive AS status,
+                 count(*) AS shard_count
+          FROM pg_dist_shard_placement placements,
+               pg_dist_node nodes
+          WHERE placements.nodename = nodes.nodename
+          GROUP BY placements.nodename, nodes.isactive
+          ORDER BY 2 DESC, 3 DESC;
+        SQL
       end
     end
   end
